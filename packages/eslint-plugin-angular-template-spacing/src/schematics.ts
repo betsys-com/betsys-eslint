e5f66a1b5afe37775256ae161874640933b4a9ac
Dawid Zbiński <dawid@zbinski.dev>
2022-09-16 12:29:41 +0200

    feat(angular-template-spacing): schematics

--- packages/eslint-plugin-angular-template-spacing/src/schematics.ts
+++ packages/eslint-plugin-angular-template-spacing/src/schematics.ts
@@ -1,23 +1,22 @@
-import * as chalk from 'chalk';
 import { name, homepage } from '@package/../package.json';
-import { isJsonArray, isJsonObject } from '@angular-devkit/core';
-import { createOrPushToArray } from '@package/utils/schematics.utils';
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { createOrPushToArray, isArray, isObject } from '@package/utils/schematics.utils';
 
 // You don't have to export the function as default. You can also have more than one rule factory per file.
-export function ngAdd(_options: any): Rule {
+export function install(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const path = '.eslint.json';
-    const pluginSettings = `plugin:${name}/recommended`;
+    const path = '.eslintrc.json';
+    const packageName = name.replace('eslint-plugin-', '');
+    const pluginSettings = `plugin:${packageName}/recommended`;
 
     if (!tree.exists(path)) {
-      console.log(chalk.bold.red(`Unable to locate ${path} file, config will not be updated. You can do it manually by following the instructions at ${homepage}`));
+      console.error(`Unable to locate ${path} file, config will not be updated. You can do it manually by following the instructions at ${homepage}`);
       return tree;
     }
 
     const json = tree.readJson(path);
-    if (!isJsonObject(json) || isJsonArray(json)) {
-      console.log(chalk.bold.red(`Your ${path} file is not a valid object. This should not be happening. No changes were made.`));
+    if (!isObject(json) || isArray(json)) {
+      console.log(`Your ${path} file is not a valid object. This should not be happening. No changes were made.`);
       return tree;
     }
 
@@ -27,15 +26,15 @@ export function ngAdd(_options: any): Rule {
     // This tries to find override settings for "*.ts" files. Will be undefined if they are not present.
     const overrides =
       json['overrides'] &&
-      isJsonArray(json['overrides']) &&
-      json['overrides'].find(override => isJsonObject(override) && isJsonArray(override['files']) && override['files'].includes('*.ts'));
+      isArray(json['overrides']) &&
+      json['overrides'].find(override => isObject(override) && isArray(override['files']) && override['files'].includes('*.html'));
 
     // If we see that there are overrides for *.ts files, we will edit those.
-    if (overrides && isJsonObject(overrides)) {
+    if (overrides && isObject(overrides)) {
       fieldsToEdit = overrides;
     }
 
-    createOrPushToArray(fieldsToEdit, 'plugins', name);
+    createOrPushToArray(fieldsToEdit, 'plugins', packageName);
     createOrPushToArray(fieldsToEdit, 'extends', pluginSettings);
 
     tree.overwrite(path, JSON.stringify(json, null, 4));
