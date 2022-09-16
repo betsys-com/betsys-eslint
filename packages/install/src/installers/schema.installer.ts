02c6e2e26a69cf305b39316c62b8f12e3ec17a28
Dawid Zbiński <dawid@zbinski.dev>
2022-09-16 12:46:46 +0200

    test(install): schema installer

--- packages/install/src/installers/schema.installer.ts
+++ packages/install/src/installers/schema.installer.ts
@@ -4,7 +4,7 @@ export class SchemaInstaller {
   static async install(packageName: string): Promise<void> {
     return new Promise(async (res, rej) => {
       exec(
-        `npx  @angular-devkit/schematics-cli ${packageName}:install`,
+        `npx @angular-devkit/schematics-cli ${packageName}:install`,
         (err, _, stderr) => {
           const errorCode = err?.code ?? 0;
           if (errorCode > 0) {
