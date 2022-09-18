e234735c3c0af830f059f385483fed846599b501
Dawid Zbiński <dawid@zbinski.dev>
2022-09-18 19:40:24 +0200

    fix(core): npm installation

--- packages/install/jest.config.cjs
+++ packages/install/jest.config.cjs
@@ -1,4 +1,4 @@
-const createBaseJestConfig = require('../../jest.helper');
+const createBaseJestConfig = require('../../jest.helper.cjs');
 const { paths } = require('./tsconfig.spec').compilerOptions;
 
 module.exports = createBaseJestConfig(paths);
