599101e9f28fb16c7ffc52a24056066db5cebba8
Dawid Zbiński <dawid@zbinski.dev>
2022-09-22 14:38:27 +0200

    fix(nestjs): no overrides yet

--- packages/eslint-config-nestjs/src/index.ts
+++ packages/eslint-config-nestjs/src/index.ts
@@ -1,13 +1,9 @@
 import type { TSESLint } from '@typescript-eslint/utils';
-import { overridesRules } from '@package/src/rules/overrides.rules';
 
 const config: TSESLint.Linter.Config = {
     extends: [
         '@betsys-eslint/typescript',
     ],
-    rules: {
-        ...overridesRules,
-    },
 };
 
 // eslint-disable-next-line import/no-default-export
